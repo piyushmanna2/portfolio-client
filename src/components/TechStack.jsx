6d4d16268fc7cd50fa88002b775b78ea29098141
Piyush Manna <w3bpiyush@gmail.com>
2024-05-15 10:00:34 +0545

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   src/components/TechStack.jsx

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -4,8 +4,10 @@ const TechStack = () => {
 
   const [skills, setSkills] = useState([]);
   const [error, setError] = useState(null);
+  const [loading , setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true)
     fetch("https://api-portfolioweb.vercel.app/app/techstack")
       .then(response => response.json())
       .then((data) => {
@@ -16,11 +18,16 @@ const TechStack = () => {
         }
       })
       .catch((err) => setError(err.message));
+      setLoading(false)
   }, []);
 
   return (
     <div className="lg:w-full lg:mb-2 mb-3">
-      <div className="bg-card-background p-4">
+      {
+        loading ?  (
+          <img src="/loader.gif" alt="loader" className="loader" />
+        ) : (
+          <div className="bg-card-background p-4">
         <p className="font-semibold text-2xl mb-4">What i know</p>
         {error ? (
           <p>{error}</p>
@@ -35,6 +42,8 @@ const TechStack = () => {
           </ul>
         )}
       </div>
+        )
+      }
     </div>
   );
 };
