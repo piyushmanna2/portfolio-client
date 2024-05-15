cecc3c5f66f7c48503f7bd8d5767ee64d311fbfa
Piyush Manna <w3bpiyush@gmail.com>
2024-05-15 10:15:11 +0545

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   src/components/About.jsx
    	modified:   src/index.css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,44 +1,44 @@
 import React, { useEffect, useState } from "react";
 
 const About = () => {
-    const [about, setAbout] = useState({});
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
+  const [about, setAbout] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        setLoading(true);
-        fetch("https://api-portfolioweb.vercel.app/app/about")
-            .then(response => response.json())
-            .then((data) => {
-                if (data.status === false) {
-                    setError(data.msg);
-                } else {
-                    setAbout(data.data[0]);
-                }
-                setLoading(false); 
-            })
-            .catch((err) => {
-                setError(err.message);
-                setLoading(false);
-            });
-    }, []);
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://api-portfolioweb.vercel.app/app/about")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === false) {
+          setError(data.msg);
+        } else {
+          setAbout(data.data[0]);
+        }
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
 
-    return (
-        <div className="lg:w-full lg:mb-3 mb-3">
-            {loading ? (
-                <img src="/loader.gif" alt="loader" className="loader" />
-            ) : (
-                error ? (
-                    <p>{error}</p>
-                ) : (
-                    <div className="bg-card-background p-4">
-                        <p className="font-semibold text-2xl">About Me</p>
-                        <p className="font-normal mt-2">{about.about}</p>
-                    </div>
-                )
-            )}
+  return (
+    <div className="lg:w-full lg:mb-3 mb-3">
+      {loading ? (
+        <div className="loader">
+          <img src="/loader.gif" alt="loader" className="loader-img" />
         </div>
-    );
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <div className="bg-card-background p-4">
+          <p className="font-semibold text-2xl">About Me</p>
+          <p className="font-normal mt-2">{about.about}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default About;
