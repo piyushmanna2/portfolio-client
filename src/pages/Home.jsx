cbe0de1085aa4962bd12ea3a39d278b42acac959
Piyush Manna <w3bpiyush@gmail.com>
2024-05-15 10:45:20 +0545

    modified:   src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 import About from "../components/About";
 import TechStack from "../components/TechStack";
 import Project from "../components/Projects";
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-const Home = () => {
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
+const Home = () => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
@@ -16,33 +16,34 @@ const Home = () => {
     }, []);
 
     return (
-        loading ? (
-            <div className="loader">
-                <img src="/loader.gif" alt="loader" />
-            </div>
-        ) : (
-            <>
-            <Navbar/>
-            <div className="container mx-auto px-4 sm:px-6 lg:px-8 mt-4">
-                <div className="lg:flex">
-                    <div className="lg:w-8/12">
-                        <h1 className="text-3xl font-semibold mb-4">Projects</h1>
-                        <div className="lg:flex lg:flex-wrap">
-                            <Project />
-                        </div>
-                    </div>
-                    <div className="lg:w-4/12 lg:pl-4 mt-12">
-                        <div className="lg:flex lg:flex-col">
-                            <About />
-                            <TechStack />
-                        </div>
-                    </div>
+        <>
+            {loading ? (
+                <div className="loader">
+                    <img src="/loader.gif" alt="loader" />
                 </div>
-            </div>
-            <Footer/>
-            </>
-        )
-
+            ) : (
+                <>
+                    <Navbar />
+                    <div className="container mx-auto px-4 sm:px-6 lg:px-8 mt-4">
+                        <div className="lg:flex">
+                            <div className="lg:w-8/12">
+                                <h1 className="text-3xl font-semibold mb-4">Projects</h1>
+                                <div className="lg:flex lg:flex-wrap">
+                                    <Project />
+                                </div>
+                            </div>
+                            <div className="lg:w-4/12 lg:pl-4 mt-12">
+                                <div className="lg:flex lg:flex-col">
+                                    <About />
+                                    <TechStack />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <Footer />
+                </>
+            )}
+        </>
     );
 };
 
