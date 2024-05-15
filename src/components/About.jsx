736cfb56ea6c7004ef283ecadd0817a4aa0b3931
Piyush Manna <w3bpiyush@gmail.com>
2024-05-15 09:58:28 +0545

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	new file:   public/loader.gif
    	modified:   src/components/About.jsx
    	modified:   src/components/TechStack.jsx
    	modified:   src/index.css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,10 @@ import React, { useEffect, useState } from "react";
 const About = () => {
     const [about, setAbout] = useState({});
     const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+        setLoading(true)
         fetch("https://api-portfolioweb.vercel.app/app/about")
             .then(response => response.json())
             .then((data) => {
@@ -15,19 +17,25 @@ const About = () => {
                 }
             })
             .catch((err) => setError(err.message));
+        setLoading(false)
     }, []);
 
     return (
         <div className="lg:w-full lg:mb-3 mb-3">
-            {error ? (
+        {loading ? (
+            <img src="/loader.gif" alt="loader" className="loader" />
+        ) : (
+            error ? (
                 <p>{error}</p>
             ) : (
                 <div className="bg-card-background p-4">
                     <p className="font-semibold text-2xl">About Me</p>
                     <p className="font-normal mt-2">{about.about}</p>
                 </div>
-            )}
-        </div>
+            )
+        )}
+    </div>
+    
     );
 };
 
