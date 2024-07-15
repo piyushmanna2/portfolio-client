3ad9af164b743f0085e9dc26c28ac4496e7ff319
piyushmanna2 <programmingpiyush@gmail.com>
2024-07-15 14:35:02 +0545

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,7 +18,7 @@ const Projects = () => {
   }, []);
 
   return (
-    <div className="lg:w-1/4 mb-4 lg:mb-0 rounded-md flex">
+    <div className="lg:w-2/4 mb-4 lg:mb-0 rounded-md flex">
       {error ? (
         <p>{error}</p>
       ) : (
